78d862921d975e6d7e290af6d31471c7743ad76b
Naputt1 <naputtalt1@gmail.com>
2026-04-24 03:28:11 +0100

    fix build

---
+++ layout-wasm/pkg/nexiq_layout_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_forcelayout_free: (a: number, b: number) => void;
+export const forcelayout_get_positions: (a: number, b: number, c: number, d: any) => void;
+export const forcelayout_new: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: any) => [number, number, number];
+export const forcelayout_run_steps: (a: number, b: number) => void;
+export const forcelayout_step: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
